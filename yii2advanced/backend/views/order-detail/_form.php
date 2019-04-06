ee6ee971305b89f5acd4b94984bcfcddd107785a
Pressiix <watcharapon.piam@gmail.com>
2019-04-06 22:08:11 +0700

    Create CRUD views for product

---
+++ yii2advanced/backend/views/order-detail/_form.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\OrderDetail */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="order-detail-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'order_id_index')->textInput() ?>
+
+    <?= $form->field($model, 'product_id_index')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
