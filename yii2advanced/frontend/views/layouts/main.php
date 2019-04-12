0264ec34050c9282c767bc245495ef99dfc55ec8
Pressiix <watcharapon.piam@gmail.com>
2019-04-12 18:49:39 +0700

    Change title for index page

--- yii2advanced/frontend/views/layouts/main.php
+++ yii2advanced/frontend/views/layouts/main.php
@@ -20,7 +20,7 @@ AppAsset::register($this);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php $this->registerCsrfMetaTags() ?>
-    <title><?= Html::encode(/*$this->title*/'Demo') ?></title>
+    <title><?= Html::encode(/*$this->title*/'ํYii2-Demo') ?></title>
     <?php $this->head() ?>
 </head>
 <body>
